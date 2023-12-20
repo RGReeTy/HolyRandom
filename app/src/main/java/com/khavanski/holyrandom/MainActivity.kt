98cc708d979e31277587efdbd2e093dc79c92856
Constantine Havansky <be.a.reety@gmail.com>
2023-12-20 23:12:27 +0100

    Fix rotation unsaved problem

--- app/src/main/java/com/khavanski/holyrandom/MainActivity.kt
+++ app/src/main/java/com/khavanski/holyrandom/MainActivity.kt
@@ -11,14 +11,29 @@ class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
     private lateinit var numbers: TextView
 
+    private val randomString = "random"
+    private var random: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
-        button.setOnClickListener {
-            val random = Random.nextInt(0, 100)
-            numbers.text = random.toString()
+        if (savedInstanceState != null) {
+            val savedRandom = savedInstanceState.getString(randomString)
+            numbers.text = savedRandom
         }
+
+        button.setOnClickListener {
+            random = Random.nextInt(0, 100).toString()
+            numbers.text = random
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        if (random != null) {
+            outState.putString(randomString, random)
+        }
+        super.onSaveInstanceState(outState)
     }
 
     private fun initViews() {

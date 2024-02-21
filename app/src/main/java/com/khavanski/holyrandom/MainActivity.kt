40c9d8ddfad3f349d3cb613bd893c3e823b6c9e6
Constantine Havansky <be.a.reety@gmail.com>
2024-02-21 19:59:12 +0100

    Added some logic

--- app/src/main/java/com/khavanski/holyrandom/MainActivity.kt
+++ app/src/main/java/com/khavanski/holyrandom/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.khavanski.holyrandom
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var button: Button
     private lateinit var numbers: TextView
+    private lateinit var timer: CountDownTimer
 
     private val randomString = "random"
     private var random: String? = null
@@ -23,11 +26,25 @@ class MainActivity : AppCompatActivity() {
             numbers.text = savedRandom
         }
 
+        timer = object : CountDownTimer(1500, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                val randomNumber = getRandomNumber()
+                numbers.text = randomNumber
+            }
+
+            override fun onFinish() {
+                val randomNumber = getRandomNumber()
+                numbers.text = randomNumber
+            }
+
+        }
+
         button.setOnClickListener {
-            random = Random.nextInt(0, 100).toString()
-            numbers.text = random
+            timer.start()
         }
     }
+
+    private fun getRandomNumber() = Random.nextInt(0, 100).toString()
 
     override fun onSaveInstanceState(outState: Bundle) {
         if (random != null) {
